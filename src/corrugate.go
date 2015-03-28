f0da2bd73d94df82031fd6856d394a81a9b6c9d0
David J Felix <felix.davidj@gmail.com>
2015-03-28 04:00:59 +0000

    Add and serve new react app

--- src/corrugate.go
+++ src/corrugate.go
@@ -14,23 +14,23 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path
 	t, _ := template.ParseFiles("index.html")
 	p := &Page{Title: title}
-	t.Execute(w, p)	
+	t.Execute(w, p)
   }
 
 func main() {
 
 	// Static file server to operate on the directory that calls it
 	static_server := http.FileServer(http.Dir("./"))
-	
+
 	// A multiplexing router to handle requests
 	router := mux.NewRouter()
-	
+
 	// Setup routes here
 	router.PathPrefix("/assets/").Handler(static_server) // will serve assets folder
 	router.HandleFunc("/", DefaultHandler) // Fallthrough to DefaultHandler
-	
+
 	// Serve the multiplexer
 	http.Handle("/", router)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":5000", nil)
 }
 
