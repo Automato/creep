07f6855543beb4cfd8cabe5acfb57482876ac9d8
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-06-07 18:30:59 -0400

    Wire up redis

--- src/cards/app.py
+++ src/cards/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask.ext.stormpath import StormpathManager, login_required
+from redis import StrictRedis
+import uuid
 
 app = Flask(__name__)
 app.debug = True #FIXME
@@ -14,32 +16,36 @@ app.config['STORMPATH_SOCIAL'] = {
     }
 }
 app.config['STORMPATH_APPLICATION'] = 'Creep'
-
+redis = StrictRedis(host=app.config.get('REDIS_HOST'), port=app.config.get('REDIS_PORT'), db=0)
 stormpath_manager = StormpathManager(app)
 
 @app.route('/', methods=['GET'])
 @login_required
 def list_cards():
-    return "cards go here"
+    return str(redis.hgetall('cards'))
 
 @app.route('/', methods=['POST'])
 @login_required
 def create_card():
-    return "add card here"
+    card_id = uuid.uuid4()
+    redis.hset('cards', str(card_id), str(request.body))
+    return "add card here. Card allegedly posted as " + str(card_id)
 
 @app.route('/<uuid:card_id>', methods=['GET'])
 @login_required
 def retrieve_card(card_id):
-    return "get card here"
+    return str(redis.hget('cards', 'card_id))
 
 @app.route('/<uuid:card_id>', methods=['PATCH', 'PUT'])
 @login_required
 def replace_card(card_id):
-    return "replace card here"
+    redis.hset('cards', str(card_id), str(request.body))
+    return "replace card here. Card allegedly replaced"
 
 @app.route('/<uuid:card_id>', methods=['DELETE'])
 @login_required
 def delete_card(card_id):
-    return "delete card here"
+    redis.hdel('cards', str(card_id))
+    return "delete card here. Card allegedly deleted"
 
 app.run(host='0.0.0.0')
