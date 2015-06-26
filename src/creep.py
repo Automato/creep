4e01438c6aceaae47b7e3976a7c20b13981be21d
David J Felix <felix.davidj@gmail.com>
2015-06-26 17:51:43 +0000

    Finish my dumb card handler.
    - Todo: switch from ghetto db to redis

--- src/creep.py
+++ src/creep.py
@@ -7,13 +7,26 @@ ghetto_db = {}
 
 class CardHandler(tornado.web.RequestHandler):
 	
-	def delete(self):
-		self.write("Delete goes here")
+	def delete(self, card_id=None):
+		global ghetto_db
+
+		if card_id:
+			if card_id in ghetto_db:
+				self.set_status(200)
+				self.write(ghetto_db.pop(card_id))
+			else:
+				self.set_status(404)
+				self.write({'error': 'card id not found'})
+		else:
+			self.set_status(405)
+			self.write({'error': 'DELTE not allowed without card id'})
+
+		raise tornado.web.Finish()
 	
 	def get(self, card_id=None):
+		global ghetto_db
+		
 		if card_id:
-			global ghetto_db
-
 			if card_id in ghetto_db:
 				self.set_status(200)
 				self.write(ghetto_db[card_id])
@@ -26,9 +39,21 @@ class CardHandler(tornado.web.RequestHandler):
 
 		raise tornado.web.Finish()
 			
+	def head(self, card_id=None):
+		global ghetto_db
 
-	def head(self):
-		self.write("Head goes here")
+		if card_id:
+			if card_id in ghetto_db:
+				self.set_status(200)
+				self.set_header('Content-Length', len(tornado.escape.encode(ghetto_db[card_id])))
+			else:
+				self.set_status(404)
+				self.set_header('Content-Length', len(tornado.escape.encode({'error': 'card id not found'})))
+		else:
+			self.set_status(200)
+			self.set_header('Content-Length', len(tornado.escape.encode(ghetto_db)))
+
+		raise tornado.web.Finish()
 		
 	def options(self, card_id=None):
 		if card_id:
@@ -47,8 +72,21 @@ class CardHandler(tornado.web.RequestHandler):
 
 		raise tornado.web.Finish()
 		
-	def patch(self):
-		self.write("Patch goes here")
+	def patch(self, card_id=None):
+		global ghetto_db
+
+		if card_id:
+			if card_id in ghetto_db:
+				self.set_status(200)
+				self.write("Patch goes here")
+			else:
+				self.set_status(404)
+				self.write({'error': 'card id not found'})
+		else:
+			self.set_status(405)
+			self.write({'error': 'PATCH not allowed without card id'})
+
+		raise tornado.web.Finish()
 		
 	def post(self, card_id=None):
 		global ghetto_db
@@ -59,19 +97,34 @@ class CardHandler(tornado.web.RequestHandler):
 				self.write({'error': 'card id already exists'})
 			else:
 				self.set_status(405)
-				self.write({'error': 'POST not allowed with cardId'})
+				self.write({'error': 'POST not allowed with card id'})
 		else:
 			self.set_status(201)
 			card_id = str(uuid.uuid4())
 			while card_id in ghetto_db:
 				card_id = str(uuid.uuid4())
 			card = tornado.escape.json_decode(self.request.body)
+			ghetto_db[card_id] = card
 			self.write({'id': card_id, 'content': card})
 
 		raise tornado.web.Finish()
 	
-	def put(self):
-		self.write("Put goes here")
+	def put(self, card_id=None):
+		global ghetto_db
+
+		if card_id:
+			if card_id in ghetto_db:
+				self.set_status(204)
+				card = tornado.escape.json_decode(self.request.body)
+				ghetto_db[card_id] = card
+			else:
+				self.set_status(404)
+				self.write({'error': 'card id not found'})
+		else:
+			self.set_status(405)
+			self.write({'error': 'PUT not allowed without card id'})
+
+		raise tornado.web.Finish()
 
 
 def main():
