4554eae85297a65d11d9bd47fbb180f61ba53be7
David J Felix <felix.davidj@gmail.com>
2015-06-26 15:03:08 +0000

    Incremental work with ghetto store

--- src/creep.py
+++ src/creep.py
@@ -1,5 +1,9 @@
+import uuid
 import tornado.ioloop
 import tornado.web
+import tornado.escape
+
+ghetto_db = {}
 
 class CardHandler(tornado.web.RequestHandler):
 	
@@ -7,21 +11,64 @@ class CardHandler(tornado.web.RequestHandler):
 		self.write("Delete goes here")
 	
 	def get(self, card_id=None):
-		self.set_status(204)
-		self.set_header('Accept', 'GET, HEAD, OPTIONS, POST')
+		if card_id:
+			global ghetto_db
+
+			if card_id in ghetto_db:
+				self.set_status(200)
+				self.write(ghetto_db[card_id])
+			else:
+				self.set_status(404)
+				self.write({'error': 'card id not found'})
+		else:
+			self.set_status(200)
+			self.write(ghetto_db)
+
 		raise tornado.web.Finish()
-		
+			
+
 	def head(self):
 		self.write("Head goes here")
 		
-	def options(self):
-		self.write("Options goes here")
+	def options(self, card_id=None):
+		if card_id:
+			global ghetto_db
+
+			if card_id in ghetto_db:
+				self.set_status(200)
+				self.set_header('Accept', 'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT')
+			else:
+				self.set_status(404)
+				self.write({'error': 'card id not found'})
+	
+		else:
+			self.set_status(200)
+			self.set_header('Accept', 'GET, HEAD, OPTIONS, POST')
+
+		raise tornado.web.Finish()
 		
 	def patch(self):
 		self.write("Patch goes here")
 		
-	def post(self):
-		self.write("Post goes here")
+	def post(self, card_id=None):
+		global ghetto_db
+
+		if card_id:
+			if card_id in ghetto_db:
+				self.set_status(409)
+				self.write({'error': 'card id already exists'})
+			else:
+				self.set_status(405)
+				self.write({'error': 'POST not allowed with cardId'})
+		else:
+			self.set_status(201)
+			card_id = str(uuid.uuid4())
+			while card_id in ghetto_db:
+				card_id = str(uuid.uuid4())
+			card = tornado.escape.json_decode(self.request.body)
+			self.write({'id': card_id, 'content': card})
+
+		raise tornado.web.Finish()
 	
 	def put(self):
 		self.write("Put goes here")
@@ -30,7 +77,7 @@ class CardHandler(tornado.web.RequestHandler):
 def main():
 	routes = [
 		(r'/cards', CardHandler),
-		(r'/cards/(?P<card_id>[^\/]+)' CardHandler)
+		(r'/cards/(?P<card_id>[^\/]+)', CardHandler)
 	]
 	application = tornado.web.Application(routes)
 	application.listen(80)
