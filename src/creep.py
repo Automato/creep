a1b895fe76aebec87dcc2d7d5f5523fdcd4ca83a
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-08-12 15:37:09 -0400

    Update creep.py

--- src/creep.py
+++ src/creep.py
@@ -11,49 +11,6 @@ ghetto_db = {
     'boards': {},
 }
 
-
-class APIHandler(tornado.web.RequestHandler):
-
-    def delete(self):
-        self.set_status(405)
-        self.write({'error': 'DELETE not allowed on API root'})
-
-    def get(self):
-        self.set_status(200)
-        self.write({
-            'cardURL': '/cards{/cardId}',
-            'categoryURL': '/categories{/categoryId}',
-            'boardURL': '/boards{/boardId}',
-        })
-
-    def head(self):
-        self.set_status(200)
-        self.set_header('Content-Length',
-            len(tornado.escape.json_encode({
-                'cardURL': '/cards{/cardId}',
-                'categoryURL': '/categories{/categoryId}',
-                'boardURL': '/boards{/boardId}',
-            }))
-        )
-        raise self.tornado.web.Finish()
-
-    def options(self):
-        self.set_status(200)
-        self.set_headers('Accept', 'GET, HEAD, OPTIONS')
-
-    def patch(self):
-        self.set_status(403)
-        self.write({'error': 'PATCH not allowed on API root'})
-
-    def post(self):
-        self.set_status(405)
-        self.write({'error': 'POST not allowed on API root'})
-
-    def put(self):
-        self.set_status(405)
-        self.write({'error': 'PUT not allowed on API root'})
-
-
 class CardHandler(tornado.web.RequestHandler):
 
     def delete(self, card_id=None):
