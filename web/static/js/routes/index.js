8722cddffb3479a59430fcc31c4f3df1c6354099
David J Felix <felix.davidj@gmail.com>
2016-05-18 23:15:36 -0400

    Setup react following blog post 2

---
+++ web/static/js/routes/index.js
@@ -0,0 +1,12 @@
+import { IndexRoute, Route } from 'react-router';
+import React from 'react';
+import MainLayout from '../layouts/main';
+import RegistrationsNew from '../views/registrations/new';
+
+export default function configRoutes(store) {
+  return (
+    <Route component={MainLayout}>
+      <Route path="/" component{RegistrationsNew} />
+    </Route>
+  );
+}
